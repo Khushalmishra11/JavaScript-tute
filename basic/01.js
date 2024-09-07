e24ffdc2c1959dc16b6b05391a267cf90d965285
Khushalmishra11 <khushalmishra261203@gmail.com>
2024-09-07 11:25:31 +0530

    upated

--- basic/01.js
+++ basic/01.js
@@ -1,1 +1,9 @@
 console.log("Khushal")
+const accountId = 144553
+let accountEmail = "khushal@google.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+console.log(accountId);
