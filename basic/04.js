c5327d7aff04a8499657ec4698abf3b9df899de7
Khushalmishra11 <khushalmishra261203@gmail.com>
2024-09-09 00:05:47 +0530

    .

---
+++ basic/04.js
@@ -0,0 +1,39 @@
+//  console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+
+ 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined = 0);
+
+
+// // === 
+
+// console.log("2" === 2);
+
+
+// # Primitive 
+
+// 7 type : string, number, boolean, null, undefined, symbol, bigInt
+
+
+//  # Reference type/ non primitive 
+// Array, objects, functions
+
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
