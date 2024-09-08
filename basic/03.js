6c669aa342c8e2f1349e5340446a8c080b1370c2
Khushalmishra11 <khushalmishra261203@gmail.com>
2024-09-08 16:58:16 +0530

    .

--- basic/03.js
+++ basic/03.js
@@ -20,3 +20,26 @@ console.log(typeof(valueInBoolean));
 // "" => false
 // "khushal " => true
 
+              // Operations
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+
+// let str1 ="hello"
+// let str2 = " Khushal "
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+console.log(true);
+
+
