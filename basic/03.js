4bb7d86f57a207ab2f5ccc8df8a8e5a826dbf93e
Khushalmishra11 <khushalmishra261203@gmail.com>
2024-09-08 11:27:01 +0530

    conversion

---
+++ basic/03.js
@@ -0,0 +1,22 @@
+let score = 33
+ 
+
+let valueInNumber = Number(score)
+
+console.log(typeof(score));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+
+let IsLoggedIn = 1
+
+let valueInBoolean = Boolean(IsLoggedIn)
+
+console.log(typeof(valueInBoolean));
+
+// 1 => true; 0 => false
+// "" => false
+// "khushal " => true
+
