87fb42a8b398e94490bf139c7bd83cfc299e2a2f
Khushal Mishra <khushalmishra261203@gmail.com>
2024-09-22 21:41:00 +0530

    .

--- basic/06_basic/02_function.js
+++ basic/06_basic/02_function.js
@@ -2,15 +2,61 @@ let a = 100
 if (true){
     let a = 10
     const b = 20  
-    console.log("INNER : ", a);
+    // console.log("INNER : ", a);
 
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array [i];
+
+
+
+// console.log(a);
+
+// console.log(b);
+
+
+function one(){
+    const username = "Khushal"
+
+    function two(){
+        const website = "youtbe"
+        console.log(username);
+
+        
+    }
+    // console.log(website);   // error
+
+    two()
     
 }
 
+// one()
 
-console.log(a);
-// console.log(b);
+if (true) {
+    const username = "Khushal"
+    if (username === "Khushal") {
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+    
+}
+
+// console.log(username);
+
+
+
+// *********************** insteresting ************************
+
+function  addOne(num){
+    return num + 1
+}
+
+addOne(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
