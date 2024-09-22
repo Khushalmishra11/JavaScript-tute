df21405fb67fcdbd3c115ca0ed5a862850fdc77f
Khushal Mishra <khushalmishra261203@gmail.com>
2024-09-22 10:01:53 +0530

    .

--- basic/06_basic/01_function.js
+++ basic/06_basic/01_function.js
@@ -22,3 +22,29 @@
 // addTwoNumbers("A", 20);
 
 
+function calculateCartPrice(val1, val2, ...num1){ // rest operator ...
+    return num1
+}
+
+console.log(calculateCartPrice(100,400,500))
+
+const uesr = {
+    username: "khushal",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anuobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(price)
+
+
+const myNewArray = [200,400,100,660]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
