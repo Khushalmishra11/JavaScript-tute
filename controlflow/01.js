aa79ede80cb40a9112bfe84c44c6c7bb59695239
Khushal Mishra <khushalmishra261203@gmail.com>
2024-09-24 22:49:10 +0530

    controlflow

---
+++ controlflow/01.js
@@ -0,0 +1,15 @@
+// If
+const isUserLoggedIn = true;
+const temperature = 41
+
+if ( 2 == "2") {
+console.log("this statement is true");
+
+}
+if ( 2 !== "2") {
+console.log("this statement is also true");
+
+}
+
+// <, >, <=, >=, ==, ===, !=, !== 
+
