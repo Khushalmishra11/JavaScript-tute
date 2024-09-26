36c986eb69481aa0326d51593f78cf90827d4d8d
Khushal Mishra <khushalmishra261203@gmail.com>
2024-09-26 23:31:02 +0530

    .

--- controlflow/01.js
+++ controlflow/01.js
@@ -1,15 +1,57 @@
 // If
-const isUserLoggedIn = true;
-const temperature = 41
 
-if ( 2 == "2") {
-console.log("this statement is true");
-
-}
-if ( 2 !== "2") {
-console.log("this statement is also true");
-
-}
+// if ( 2 == "2") {
+    // console.log("this statement is true");
+    
+    // }
+    // if ( 2 !== "2") {
+        // console.log("this statement is also true");
+        
+        // }
+// const isUserLoggedIn = true;
+// const temperature = 41
+// if (tempeorature > 50) {
+//     console.log("It's too hot outside");
+    
+// } else {
+//     console.log("temperature is more than 50");
+    
+// }
 
 // <, >, <=, >=, ==, ===, !=, !== 
 
+
+const balance = 1000
+// if (balance > 500 ) console.log("test"), console.log("test2");
+
+// if (balance < 500 ) {
+//     console.log("less than");
+
+// }else if(balance < 750){
+//     console.log("less than 750");
+    
+// }else if (balance < 900 ) {
+//     console.log("less than 900");
+    
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+
+
+const userLoggedIn = true;
+const debitCard = true
+const loggedInfromGoogle = false
+const loggedInfromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("You can make a purchase");
+    
+}
+
+if (loggedInfromGoogle || loggedInfromEmail) {
+    console.log("user logged in");
+    
+}
