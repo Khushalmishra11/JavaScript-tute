11e0b5e85369e32fb16aec583564a3056ad72c4d
Khushal Mishra <khushalmishra261203@gmail.com>
2024-09-29 23:03:49 +0530

    loops

--- controlflow/loops.js
+++ controlflow/loops.js
@@ -1,6 +1,7 @@
 // for loop
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+for (let i = 0; i < 10 ; i++) {
+    const element = i;
+    console.log(element);
     
 }
