957cbb9ceb4bfd616e9926137cca869b4d56a057
Khushal Mishra <khushalmishra261203@gmail.com>
2024-10-25 23:02:27 +0530

    updated

--- controlflow/loops.js
+++ controlflow/loops.js
@@ -1,7 +1,51 @@
 // for loop
 
-for (let i = 0; i < 10 ; i++) {
-    const element = i;
-    console.log(element);
+// for (let i = 0; i < 10 ; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// for of loop
+// [ "", "" , " "]
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
     
 }
+
+
+// const greeting = "Hello World!"
+// for (const greet of greeting){
+//     console.log(`Each char is ${greet}`)
+    
+    
+// }
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('US', "United States of america")
+map.set('FR', "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key);
+    
+    
+}
+
+const myObject = {
+    game1 : 'NFS',
+    game2 : 'spiderman'
+}
+
+// for (const [key] of myObject) {
+//     console.log(key);
+    
+// }
+
---
+++ controlflow/loops2.js
@@ -0,0 +1,25 @@
+const myObject = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb :  'Ruby',
+    swift : 'Swift by apple'
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+    
+
+// }
+
+
+const programming = ['js', 'ruby','python','java']
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+const coding = ['js', 'ruby','python','java']
+
+coding.forEach(i => {
+    
+});
